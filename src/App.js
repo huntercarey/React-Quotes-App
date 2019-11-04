80b559c601dece476079ff1d667dcf9c9bdf4392
Hunter Carey <huntercarey902@gmail.com>
2019-11-04 13:21:14 -0600

    added quotes.js and set the array of quotes to a const 'quotes' and imported it into App.js and QuoteBox.js // set up a constructor and set the default state to quotes[0].quote and quotes[0].author // added a quote and author prop to the QuoteBox class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import QuoteBox from './QuoteBox/QuoteBox';
 import './App.css';
+import quotes from './QuoteBox/quotes';
 
 class App extends Component {
+  //Setting up the constructor function so we can set our default state
+  constructor(props){
+    super(props);
+    //Set the first quote and author as our default state
+    this.state= {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+  }
   render() {
+    console.log(quotes[0].author);
+    console.log(quotes[0].quote);
     return (
       <div>
-        <QuoteBox />
+        <QuoteBox 
+        quote={this.state.quote}
+        author={this.state.author}
+        />
       </div>
     );
   }
--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import quotes from './quotes';
 
 class QuoteBox extends Component {
     render() {
         return(
             <div>
-                <h1>QuoteBox</h1>
+                <h1>{this.props.quote}</h1>
+                <h1>{this.props.author}</h1>
             </div>
         );
     }
